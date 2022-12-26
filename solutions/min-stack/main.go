fe2bd5a733aeb9a7413cd1266fcac31e0dee85f4
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-12-26 19:37:26 +0700

    Add the solution code for Min Stack

---
+++ solutions/min-stack/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func main() {
+	minStack := Constructor()
+	minStack.Push(-2)
+	minStack.Push(0)
+	minStack.Push(-3)
+	fmt.Println(minStack.GetMin())
+	minStack.Pop()
+	fmt.Println(minStack.Top())
+	fmt.Println(minStack.GetMin())
+}
+
+type MinStack struct {
+	items []int
+	min   map[int]int
+}
+
+func Constructor() MinStack {
+	return MinStack{min: make(map[int]int)}
+}
+
+func (this *MinStack) Push(val int) {
+	this.items = append(this.items, val)
+
+	if len(this.items) == 1 {
+		this.min[len(this.items)] = val
+	} else {
+		prevMin := this.min[len(this.items)-1]
+		var min int
+
+		if val < prevMin {
+			min = val
+		} else {
+			min = prevMin
+		}
+
+		this.min[len(this.items)] = min
+	}
+}
+
+func (this *MinStack) Pop() {
+	this.items = this.items[:len(this.items)-1]
+}
+
+func (this *MinStack) Top() int {
+	return this.items[len(this.items)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	return this.min[len(this.items)]
+}
