dad4dd056875c6d16b96bd38b4d2d3866ee7b789
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-01-03 17:34:25 +0700

    Add the solution code for Smallest Even Multiple

---
+++ solutions/smallest-even-multiple/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(smallestEvenMultiple(5))
+	fmt.Println(smallestEvenMultiple(6))
+}
+
+func smallestEvenMultiple(n int) int {
+	if n%2 == 0 {
+		return n
+	}
+
+	return n * 2
+}
