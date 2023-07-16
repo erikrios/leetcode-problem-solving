44e534e55928e36535cb6cefd44224afc4065c22
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-07-16 07:34:29 +0700

    Improve the code readability

--- solutions/valid-parentheses/main.go
+++ solutions/valid-parentheses/main.go
@@ -1,44 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
+	fmt.Println(isValid("()"))
 	fmt.Println(isValid("()[]{}"))
+	fmt.Println(isValid("(]"))
+}
+
+var openingBraces = map[byte]byte{
+	'(': ')',
+	'[': ']',
+	'{': '}',
+}
+
+var closingBraces = map[byte]byte{
+	')': '(',
+	']': '[',
+	'}': '{',
 }
 
 func isValid(s string) bool {
-	openingBraces := []byte{'(', '{', '['}
-	closingBraces := []byte{')', '}', ']'}
-
 	stack := &stack{}
 
 	for i := 0; i < len(s); i++ {
-		if s[i] == openingBraces[0] || s[i] == openingBraces[1] || s[i] == openingBraces[2] {
-			stack.Push(s[i])
-		} else if (s[i] == closingBraces[0] && !stack.IsEmpty() && stack.Peek() == openingBraces[0]) || (s[i] == closingBraces[1] && !stack.IsEmpty() && stack.Peek() == openingBraces[1]) || (s[i] == closingBraces[2] && !stack.IsEmpty() && stack.Peek() == openingBraces[2]) {
-			stack.Pop()
+		brace := s[i]
+
+		if isOpeningBrace(brace) {
+			stack.Push(brace)
 		} else {
-			stack.Push(s[i])
+			openingPair := closingBraces[brace]
+			v, err := stack.Peek()
+			if err != nil || v != openingPair {
+				return false
+			}
+
+			stack.Pop()
 		}
 	}
 
 	return stack.IsEmpty()
 }
 
+func isOpeningBrace(brace byte) bool {
+	_, ok := openingBraces[brace]
+	return ok
+}
+
+var stackEmptyErr = errors.New("stack is empty")
+
 type stack struct {
 	braces []byte
 }
 
-func (s *stack) Pop() {
+func (s *stack) Pop() error {
+	if s.IsEmpty() {
+		return stackEmptyErr
+	}
+
 	s.braces = s.braces[0 : len(s.braces)-1]
+	return nil
 }
 
 func (s *stack) Push(brace byte) {
 	s.braces = append(s.braces, brace)
 }
 
-func (s *stack) Peek() byte {
-	return s.braces[len(s.braces)-1]
+func (s *stack) Peek() (byte, error) {
+	if s.IsEmpty() {
+		return ' ', stackEmptyErr
+	}
+
+	return s.braces[len(s.braces)-1], nil
 }
 
 func (s *stack) IsEmpty() bool {
