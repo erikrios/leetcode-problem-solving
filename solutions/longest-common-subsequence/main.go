a66ceaa49aa51a39a144f047c056daadd40be5de
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2024-01-25 10:51:23 +0700

    Add the solution code for Longest Common Subsequence

--- solutions/longest-common-subsequence/main.go
+++ solutions/longest-common-subsequence/main.go
@@ -10,41 +10,28 @@ func main() {
 }
 
 func longestCommonSubsequence(text1, text2 string) int {
-	results1 := backtrack(text1, 0, make([]byte, 0))
-	results2 := backtrack(text2, 0, make([]byte, 0))
+	m := len(text1)
+	n := len(text2)
 
-	var max int
-	for k := range results2 {
-		if _, ok := results1[k]; ok {
-			if n := len(k); n > max {
-				max = n
+	matrix := make([][]int, 0, m+1)
+	for i := 0; i < cap(matrix); i++ {
+		cols := make([]int, n+1)
+		matrix = append(matrix, cols)
+	}
+
+	for i := len(matrix) - 2; i >= 0; i-- {
+		for j := len(matrix[0]) - 2; j >= 0; j-- {
+			if text1[i] == text2[j] {
+				matrix[i][j] = 1 + matrix[i+1][j+1]
+			} else {
+				max := matrix[i][j+1]
+				if bottom := matrix[i+1][j]; bottom > max {
+					max = bottom
+				}
+				matrix[i][j] = max
 			}
 		}
 	}
 
-	return max
-}
-
-func backtrack(text string, index int, chars []byte) map[string]struct{} {
-	n := len(text)
-	if index == n {
-		results := map[string]struct{}{string(chars): {}}
-		return results
-	}
-
-	char := text[index]
-
-	// Take
-	chars = append(chars, char)
-	takeRes := backtrack(text, index+1, chars)
-
-	// Not Take
-	chars = chars[0 : len(chars)-1]
-	notTakeRes := backtrack(text, index+1, chars)
-
-	for k, v := range notTakeRes {
-		takeRes[k] = v
-	}
-
-	return takeRes
+	return matrix[0][0]
 }
